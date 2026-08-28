a1265260697dc76870c8490a43a8c845e649d39b
Wesley Alonzo <wesleyalonzo10@gmail.com>
2026-08-28 00:49:08 -0600

    Initial Spring Boot and Maven project structure

---
+++ src/main/java/com/laboratorio_v/spring_apis/SpringApisApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio_v.spring_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laboratorio_v/spring_apis/SpringApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorio_v.spring_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
